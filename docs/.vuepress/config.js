72ff84ad1c91549a107d55bf32c93d3d06c1d056
Reinier Sierag <reinier@sierag.nl>
2025-04-16 18:22:10 +0200

    Nieuwe eerste versie

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+
+export default defineUserConfig({
+  base: '/coachball-info/',
+  lang: 'nl-NL',
+
+  title: 'CoachBall',
+  description: 'CoachBall documentatie',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: webpackBundler(),
+})
